6f87dce3faf67d3eecc7d4d1a1d6a95dc822e5e8
段诗闻 <yakii9@vip.qq.com>
2026-07-08 10:04:17 +0800

    Vendor patched MailCore umbrella header

---
+++ Vendor/MailCoreSPM/artifacts/MailCore.xcframework/macos-arm64_x86_64/MailCore.framework/Versions/A/Headers/MailCore.h
@@ -0,0 +1,27 @@
+//
+//  mailcore.h
+//  mailcore2
+//
+//  Created by DINH Viêt Hoà on 1/10/13.
+//  Copyright (c) 2013 MailCore. All rights reserved.
+//
+
+#ifndef MAILCORE_MAILCORE_H
+
+#define MAILCORE_MAILCORE_H
+
+#include <MailCore/MCCore.h>
+#include <MailCore/MCAsync.h>
+#include <MailCore/MCObjC.h>
+#include <MailCore/MCIMAPConnectOperation.h>
+#include <MailCore/MCIMAPNoopOperation.h>
+#include <MailCore/MCMXRecordResolverOperation.h>
+#include <MailCore/MCMainThread.h>
+#include <MailCore/MCONNTPDisconnectOperation.h>
+#include <MailCore/MCOPOPNoopOperation.h>
+#include <MailCore/MCOSMTPLoginOperation.h>
+#include <MailCore/MCPOPNoopOperation.h>
+#include <MailCore/MCSMTPNoopOperation.h>
+#include <MailCore/MCSizeFormatter.h>
+
+#endif
